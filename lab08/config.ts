263c4b3e570146a90a7158723c4819f261029fa4
non rabbit hole <72589672+nonrabbithole@users.noreply.github.com>
2025-03-14 21:19:11 +0800

    uploaded lab08

---
+++ lab08/config.ts
@@ -0,0 +1,10 @@
+const config = {
+    host: "localhost",
+    port: 5432,
+    user: "postgres",
+    password: "webapi2025",
+    database: "webapilab",
+    connection_limit:100
+}
+
+export default config;
