1e07ec60e181f07becec938c6ef81b1b02a4156f
non rabbit hole <72589672+nonrabbithole@users.noreply.github.com>
2025-03-14 15:15:51 +0800

    Updated Lab 7b & 8
    Lab 7b - schema
    Lab 8 - api doc

--- lab08/routers/articles.ts
+++ lab08/routers/articles.ts
@@ -1,35 +1,46 @@
 import Router, {RouterContext} from 'koa-router';
 import bodyParser from 'koa-bodyparser';
-import * as model from '../models/users';
+import * as model from '../models/articles';
 import { basicAuth } from '../controllers/auth';
-import { validateUser } from '../controllers/validation';
+import { validateArticle } from '../controllers/validation';
 
-const router = new Router({prefix: '/api/v1/users'});
+const router = new Router({prefix: '/api/v1/articles'});
 
 const getAll = async (ctx:RouterContext, next: any) => {
-    let users = await model.getAll();
-    ctx.body = users;
+    let articles = await model.getAll();
+    ctx.body = articles;
     await next();
 }
 
 const getById = async (ctx:RouterContext, next: any) => {
     let id = +ctx.params.id;
-    let user = await model.getById(id);
-    if(user.length){
-        ctx.body = user[0];
+    let article = await model.getById(id);
+    if(article.length){
+        ctx.body = article[0];
     } else {
         ctx.status = 404;
     }
     await next();
 }
-/*
-testing
+
+/*testing data for function validateArticle: 
 {
-    "username": "Jackson",
-    "email": "jack@gmail.com"
+    "title": "lab07-b",
+    "allText": "lab07-b",
+}
+{
+    "title": "lab07-b",
+    "allText": "lab07-b",
+    "authorID": "1"
+}
+{
+    "title": "lab07-b",
+    "allText": "lab07-b",
+    "authorID": 1
 }
 */
-const createUser = async (ctx:RouterContext, next: any) => {
+
+const createArticle = async (ctx:RouterContext, next: any) => {
     const body = ctx.request.body;
     let result = await model.add(body);
     if(result.status == 201){
@@ -43,7 +54,7 @@ const createUser = async (ctx:RouterContext, next: any) => {
     await next();
 }
 
-const updateUser = async (ctx:RouterContext, next: any) => {
+const updateArticle = async (ctx:RouterContext, next: any) => {
     let id = +ctx.params.id;
     const body = ctx.request.body;
     let result = await model.update(id,body);
@@ -57,13 +68,13 @@ const updateUser = async (ctx:RouterContext, next: any) => {
     await next();
 }
 
-const deleteUser = async (ctx:RouterContext, next: any) => {
+const deleteArticle = async (ctx:RouterContext, next: any) => {
     let id = +ctx.params.id;
     let result = await model.del(id);
     if(result.status == 201){
         ctx.status = 201;
         ctx.body = {
-            message: "Removed user " + id
+            message: "Removed article " + id
         };
     }else{
         ctx.status = 500;
@@ -73,10 +84,10 @@ const deleteUser = async (ctx:RouterContext, next: any) => {
 }
 
 
-router.get('/', basicAuth, getAll);
+router.get('/', getAll);
 router.get('/:id([0-9]{1,})', basicAuth, getById);
-router.post('/', bodyParser(), basicAuth, validateUser, createUser);
-router.put('/:id([0-9]{1,})', basicAuth, bodyParser(), validateUser, updateUser);
-router.delete('/:id([0-9]{1,}), basicAuth', deleteUser);
+router.post('/', basicAuth, bodyParser(), validateArticle, createArticle);
+router.put('/:id([0-9]{1,})', basicAuth, bodyParser(), validateArticle, updateArticle);
+router.delete('/:id([0-9]{1,})', basicAuth, deleteArticle);
 
 export { router };
